146aa426f19373edae1cd7d427e57e4df34e3b74
Ivan Terekh <ivan.terekh@gmail.com>
2019-03-04 18:24:13 +0300

    Added config and env.

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,12 @@ RUN apk update && apk add --no-cache ca-certificates git make tzdata \
 
 FROM scratch
 
-WORKDIR /go-skeleton/
-COPY --from=builder /go-skeleton/app /go-skeleton/app 
 COPY --from=builder /etc/passwd /etc/passwd
 COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+WORKDIR /go-skeleton/
+COPY --from=builder /go-skeleton/app /go-skeleton/app
+COPY config.json .
 EXPOSE 8080
 USER appuser
 
--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ivanterekh/go-skeleton/server"
+	"github.com/spf13/viper"
 	"github.com/xlab/closer"
+	"log"
 )
 
 func main() {
-	server.Start(":8080")
+	err := initConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.Start(":" + viper.GetString("port"))
 	closer.Hold()
+}
+
+func initConfig() error {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("could not read config file: %v", err)
+	}
+	return nil
 }
