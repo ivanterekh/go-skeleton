fe34e296dfa4970ce6233642187458e10e3c8bed
Ivan Terekh <ivan.terekh@gmail.com>
2019-03-06 11:19:07 +0300

    Simplified working with env.

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 )
 
 func main() {
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "unknown"
+	}
+	log.Printf("running app in %s environment", env) // TODO: change logging
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT must be provided as environment variable")
+		log.Fatal("PORT must be provided as environment variable") // TODO: change logging
 	}
 
+	log.Printf("starting server on port %v", port) // TODO: change logging
 	server.Start(":" + port)
 	closer.Hold()
 }
