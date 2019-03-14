75cef047cc7b15ea59ca14002242db38557f387c
Ivan Terekh <ivan.terekh@gmail.com>
2019-03-14 12:46:16 +0300

    Db (#16)
    * Start adding zap
    
    * Implement graceful shutdown from scratch. (#12)
    
    * Implement graceful shutdown from scratch.
    * Change port to address.
    * Simplified reading configuration.
    * Move env vars work to env package.
    * Remove unnecessary code.
    
    * Add db connection.
    
    * Delete .env.dev
    
    * Delete .env.prod
    
    * Delete .env.stage
    
    * Restructured code.

---
+++ db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	// Imported for using postgres driver.
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+
+	"github.com/ivanterekh/go-skeleton/env"
+)
+
+var cfg = struct {
+	user, password, address, name, sslMode string
+}{
+	user:     env.GetString("DB_USER", "postgres"),
+	password: env.GetString("DB_PASSWORD", "password"),
+	address:  env.GetString("DB_ADDRESS", "localhost"),
+	name:     env.GetString("DB_NAME", "goskeleton"),
+	sslMode:  env.GetString("DB_SSL_MODE", "disable"),
+}
+
+// New creates db instance and pings it.
+func New() (*sql.DB, error) {
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=%s",
+		cfg.user,
+		cfg.password,
+		cfg.address,
+		cfg.name,
+		cfg.sslMode,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect to database")
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, errors.Wrap(err, "could not ping database")
+	}
+
+	return db, nil
+}
